6bce9b17bcc699194d94dff3ac439685ebb8f6a0
Mekarosi <mekarosi_assa@yahoo.com>
2022-03-20 05:39:04 -0700

    Create components and add color theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Navbar } from './components/Navbar'
+import { Routes } from './components/Routes';
+import { Footer } from './components/Footer';
 
-
+import './styles/theme.css'
 import './App.css';
 
 const App = () => {
+  
+  const [darkTheme, setDarkTheme] = useState('false');
   return (
-    <div>
+    <div className={`App ${darkTheme ? 'dark' : 'light'}` }>
         <h1>Welcome to gogle</h1>
     </div>
   );
