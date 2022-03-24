27db2738f87fefd154b8e4faf36a7710fe978115
Mekarosi <mekarosi_assa@yahoo.com>
2022-03-24 07:13:36 -0700

    Add media query break point for devices

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,21 @@ import { Search } from './Search'
 
 export const Navbar = ({ darkTheme, setDarkTheme }) => {
   return (
-    <div className='navbar-body .dark'>
-        <div className="navbar-div .dark">
-            <Link to='/' >
+    <div className='navbar-body .dark' >
+        <div className="navbar-div .dark" style={{ justifyContent: 'space-between', display: 'flex', alignItems: 'center' }}>
+           <div>
+           <Link to='/' style={{ textDecoration: 'none' }}>
                 <p className='navbar-links'>
-                    Gogle <span>🔎</span>
+                    Gog <span>🔎</span>
                 </p>
             </Link>
+           </div>
+            <div>
             <button type='button' className='toggle-btn dark' onClick={() => setDarkTheme(!darkTheme)}>
                  {darkTheme ? 'Light 💡' : 'Dark 🌙'}
             </button>
+            </div>
+            
         </div>
         <Search />
     </div>
